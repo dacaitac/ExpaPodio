2ff697ba298113f155059a993d77952ecac81920
Daniel Caita <dacaitac@unal.edu.co>
2018-10-14 15:45:45 -0500

    Create user on EXPA

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const fs = require('fs')
-const config = JSON.parse(fs.readFileSync('./config.json'))
-var expa = require('node-gis-wrapper')(config.expa.username, config.expa.password);
-expa.get('current_person.json').then(console.log).catch(console.log);
+let user = {
+  "user":{
+    "first_name": "Michael",
+    "last_name": "Victor",
+    "email": "i7.danielcc+test6@gmail.com",
+    "country_code": "+91",
+    "phone": "9845286710",
+    "password": "Aiesec123",
+    "lc": 1395
+  }
+}
+
+var request = require('request');
+
+var options = {
+  uri: 'https://auth.aiesec.org/users.json',
+  method: 'POST',
+  form: user,
+  json: true
+};
+
+request(options, function (error, response, body) {
+  console.log(response.statusCode);
+  if (!error && response.statusCode == 200) {
+    console.log(body.id) // Print the shortened url.
+  }
+});
