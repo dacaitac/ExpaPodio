d6c0d22924d7246067887a7b0e2b09860ac41064
Daniel Caita <dacaitac@unal.edu.co>
2018-10-29 22:54:22 -0500

    Change port

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/newItem', (req, res) => {
 if (module === require.main) {
   // [START server]
   // Start the server
-  const server = app.listen(process.env.PORT || 8080, () => {
+  const server = app.listen(process.env.PORT || 80, () => {
     const port = server.address().port;
     console.log(`App listening on port ${port}`);
   });
