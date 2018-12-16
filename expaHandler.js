4feaa053bc0c2b633cd0f60e140f6ebf6cee24df
Daniel Caita <dacaitac@unal.edu.co>
2018-12-16 16:55:03 -0500

    Set for cron

--- expaHandler.js
+++ expaHandler.js
@@ -3,7 +3,9 @@
 const fs      = require('fs')
 const config  = JSON.parse(fs.readFileSync('./config.json'))
 var   expa    = require('node-gis-wrapper')(config.expa.username, config.expa.password);
-const podio     = require("./podioHandler")
+
+const podio   = require("./podioHandler"),
+      igv     = require('./igvPodio');
 
 let token
 async function getToken(){
@@ -12,25 +14,6 @@ async function getToken(){
     .catch(console.log)
   console.log(token);
 }
-getToken()
+// getToken()
 
-let s = "s"
-
-let rq = {
-  "fields": [
-    {
-      "status": "active",
-      "type": "text",
-      "field_id": 180880007,
-      "label": "Texto",
-      "values": [
-        {
-          "value": s
-        }
-      ],
-      "external_id": "texto"
-    }
-  ]
-}
-
-// podio.updateItem(989827985, rq)
+igv.fetchOpps()
